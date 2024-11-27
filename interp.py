0c8cc11149b3e4d192fa2befb9f5b442ea727ff3
linvegas <saulo.costam@gmail.com>
2024-11-26 22:49:07 -0300

    Interpreting if conditions
    There is now a 'Program' class just for 'self' sake.

--- interp.py
+++ interp.py
@@ -3,64 +3,92 @@ import sys
 
 VARS = {}
 
-def eval_expr(expr: str) -> int:
-    stack = []
+class Program:
+    count = 0
+    lines: list[str]
 
-    for token in expr.split():
-        if token.isdigit(): stack.append(int(token))
+    def __init__(self, lines: list[str]):
+        self.lines = lines
 
-        elif token == "+":
-            result = 0
-            for num in stack: result += num
-            stack.clear()
-            stack.append(result)
+    def eval_expr(self, expr: str) -> int:
+        stack = []
 
-        elif token == "-":
-            result = stack[0]
-            for i, num in enumerate(stack):
-                if i == 0: continue
-                result -= num
-            stack.clear()
-            stack.append(result)
+        for token in expr.split():
+            if token.isdigit(): stack.append(int(token))
 
-        elif token == "*":
-            result = 1
-            for num in stack: result *= num
-            stack.clear()
-            stack.append(result)
+            elif token == "+":
+                result = 0
+                for num in stack: result += num
+                stack.clear()
+                stack.append(result)
 
-        elif token == "/":
-            result = stack[0]
-            for i, num in enumerate(stack):
-                if i == 0: continue
-                result /= num
-            stack.clear()
-            stack.append(result)
+            elif token == "-":
+                result = stack[0]
+                for i, num in enumerate(stack):
+                    if i == 0: continue
+                    result -= num
+                stack.clear()
+                stack.append(result)
 
-        elif token in VARS: stack.append(VARS[token])
+            elif token == "*":
+                result = 1
+                for num in stack: result *= num
+                stack.clear()
+                stack.append(result)
 
-        else: print("UNDENTIFIED TOKEN:", f"'{token}'")
+            elif token == "/":
+                result = stack[0]
+                for i, num in enumerate(stack):
+                    if i == 0: continue
+                    result /= num
+                stack.clear()
+                stack.append(result)
 
-    return stack[0]
+            elif token in VARS: stack.append(VARS[token])
 
-def eval_stmt(stmt: str) -> None:
-    (lhs, rhs) = stmt.split(maxsplit=1)
+            else: print("UNIDENTIFIED TOKEN:", f"'{token}'")
 
-    match lhs:
-        case "print": print(eval_expr(rhs.strip()))
-        case _:
-            expr = rhs.split("=")[1].strip()
-            VARS[lhs.strip()] = eval_expr(expr)
+        return stack[0]
+
+    def eval_cond(self, cond: str) -> bool:
+        (lhs, operand, rhs) = cond.split()
+        match operand:
+            case ">": return self.eval_expr(lhs) > self.eval_expr(rhs)
+            case _: print(f"UNIDENTIFIED OPERAND: {operand}")
+
+    def eval_stmt(self, stmt: str) -> None:
+        # (lhs, rhs) = stmt.split(maxsplit=1)
+        lhs = stmt.split()[0]
+
+        match lhs:
+            case "print":
+                print(self.eval_expr(stmt.split(maxsplit=1)[1].strip()))
+                self.count += 1
+            case "if":
+                if self.eval_cond(stmt.split(maxsplit=1)[1].strip()): self.count += 1
+                else:
+                    while self.lines[self.count].split()[0] != "end": self.count += 1
+                    self.count += 1
+            case "end": self.count += 1
+            case _:
+                expr = stmt.split(maxsplit=1)[1].split("=")[1].strip()
+                VARS[lhs.strip()] = self.eval_expr(expr)
+                self.count += 1
+
+    def run(self):
+        while self.count < len(self.lines) - 1:
+            if self.lines[self.count].strip() != "": self.eval_stmt(self.lines[self.count])
+            else: self.count += 1
+
 
 def main():
     if len(sys.argv) < 2:
-        print("USAGE: lang.py <file>")
+        print("USAGE: interp.py <file>")
         exit(1)
 
-    source = io.open(sys.argv[1]).read()
+    source = io.open(sys.argv[1]).read().split("\n")
 
-    for line in source.split("\n"):
-        if line.strip() != "": eval_stmt(line)
+    Program(source).run()
 
 if __name__ == "__main__":
     main()
